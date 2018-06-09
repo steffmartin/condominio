e8478892a343339d21a2e2fe1b44c4b92ee2ac3e
steff <steff@Computador>
2018-06-09 19:31:21 -0300

    Front-end do login.

--- Condomínio/src/main/java/app/condominio/controller/InicioController.java
+++ Condomínio/src/main/java/app/condominio/controller/InicioController.java
@@ -3,15 +3,21 @@ package app.condominio.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class InicioController {
 
-	@GetMapping({ "/", "" })
-	public ModelAndView home(ModelMap model) {
-		model.addAttribute("titulo", "Página Inicial");
+	@GetMapping({ "/", "", "/home", "/inicio" })
+	public ModelAndView inicio(ModelMap model) {
 		model.addAttribute("conteudo", "inicio");
+		return new ModelAndView("site/layout", model);
+	}
+
+	@GetMapping({ "/entrar", "/login" })
+	public ModelAndView preLogin(ModelMap model) {
+		model.addAttribute("conteudo", "login");
 		return new ModelAndView("site/layout", model);
 	}
 
